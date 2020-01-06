7aa6e661c4c67aa479513bfb512160d382bb7e20
Igor Todorovic <igor333m@yahoo.com>
2020-01-05 21:23:34 -0800

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    inputValue: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.inputValue);
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Hello</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.inputValue} onChange={this.handleChange} placeholder="Search.." name="search" />
+            <button type="submit" value="Submit">Submit</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
